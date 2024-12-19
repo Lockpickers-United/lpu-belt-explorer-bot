6dc1a796796ba1213d733dc000f7be14a1dbe1d7
mgsecure <125900659+mgsecure@users.noreply.github.com>
2024-12-19 11:17:00 -0800

    update /rafl, add json of words for random url

--- src/commands/data/rafl.js
+++ src/commands/data/rafl.js
@@ -1,18 +1,22 @@
 const {SlashCommandBuilder} = require('discord.js')
 const dayjs = require('dayjs')
+const words = require('../../util/words.json')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('rafl')
         .setDescription('RAFL!'),
 
+
     async execute(interaction) {
         const {user: {username, discriminator}} = interaction
         const requestor = `${username}#${discriminator}`
         console.log(`${dayjs().toISOString()} Processing command: /rafl requestor=${requestor}`)
 
+        const word = words[Math.floor(Math.random() * words.length)].replaceAll(' ','-')
+
         const response = [
-            `**RAFL!**: https://share.lpubelts.com/rafl`
+            `**RAFL!**: https://share.lpubelts.com/rafl?w=${word}!`
         ].join('\n')
         await interaction.reply(response)
     }
